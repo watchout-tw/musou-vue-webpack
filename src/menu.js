8a9a56f0d3af1ba9f0038fbe96d9cece12e57dfa
chihao <chihaoyo@gmail.com>
2017-07-09 11:14:12 +0800

    Switch Draw and Map

--- src/menu.js
+++ src/menu.js
@@ -37,18 +37,6 @@ var menu = [
     ]
   },
   {
-    id: 'draw',
-    title: '不然你來畫畫看啊',
-    description: '不靠感覺，靠資料、靠數據的新聞，有時候並不符合期待。你也來試試？',
-    component: Draw,
-    pages: [
-      {
-        id: 'tsai-first-year',
-        title: '蔡總統的第一年'
-      }
-    ]
-  },
-  {
     id: 'map',
     title: '啊不是很有國際觀',
     description: '世界各地的人，說著不同的語言，讀著不同的報導，過著不同的生活。我們對他們了解多少？他們又是如何認識、看待台灣的？',
@@ -57,6 +45,18 @@ var menu = [
       {
         id: '748',
         title: '同志結婚會滅國？醒醒吧，你沒有____'
+      }
+    ]
+  },
+  {
+    id: 'draw',
+    title: '不然你來畫畫看啊',
+    description: '不靠感覺，靠資料、靠數據的新聞，有時候並不符合期待。你也來試試？',
+    component: Draw,
+    pages: [
+      {
+        id: 'tsai-first-year',
+        title: '蔡總統的第一年'
       }
     ]
   }
